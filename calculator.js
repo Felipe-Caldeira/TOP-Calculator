d3b537523b986decb012fada9ddac98efdb23a6d
Felipe Caldeira <hms1730105@gmail.com>
2020-07-20 01:23:38 -0500

    Finish more functionality and display features.

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ const history = document.querySelector(".history");
 let displayVal = "0";
 let prevVal = "0";
 let selectedOperator = "";
+let pastEquateVal;
 let startNewNum = true;
 let enterClicked = false;
 
@@ -16,7 +17,11 @@ function setup() {
         op.addEventListener("click", insertOperator);
     });
 
+    document.querySelector(".decimal").addEventListener("click", insertDecimal);
+
     document.querySelector(".enter").addEventListener("click", equate);
+
+    document.querySelector(".delete").addEventListener("click", deleteChar);
 }
 
 function insertNumber(event) {
@@ -28,7 +33,8 @@ function insertNumber(event) {
         displayVal = "";
         startNewNum = false;
     }
-    displayVal += event.target.innerText;
+    if (displayVal.length > 20) return;
+    (displayVal == "0") ? displayVal = event.target.innerText: displayVal += event.target.innerText;
     updateDisplay();
 }
 
@@ -40,26 +46,41 @@ function insertOperator(event) {
     }
     if (!startNewNum) {
         let oldOperator = selectedOperator;
-        selectedOperator = event.target.innerText; 
+        selectedOperator = event.target.innerText;
         history.innerText += displayVal + selectedOperator;
         displayVal = operate(oldOperator, prevVal, displayVal);
         prevVal = displayVal;
         updateDisplay();
         startNewNum = true;
-    }
-    else {
+    } else {
         selectedOperator = event.target.innerText;
-        history.innerText = (history.innerText.slice(0, -1) || 0) + selectedOperator; 
+        history.innerText = (history.innerText.slice(0, -1) || 0) + selectedOperator;
     }
 }
 
 function insertDecimal(event) {
-
+    if (displayVal.includes(".") || displayVal.length > 20) return;
+    if (startNewNum) {
+        displayVal = "0."
+        startNewNum = false;
+    }
+    else {
+        displayVal += ".";
+    }
+    updateDisplay();
 }
 
 function equate(event) {
+    if (Debs()) return;
+    if (enterClicked) {
+        history.innerText = prevVal + selectedOperator + pastEquateVal + "=";
+        displayVal = operate(selectedOperator, displayVal, pastEquateVal);
+        prevVal = displayVal;
+        updateDisplay();
+    }
     if (!startNewNum) {
         history.innerText += displayVal + "=";
+        pastEquateVal = displayVal;
         displayVal = operate(selectedOperator, prevVal, displayVal);
         prevVal = displayVal;
         updateDisplay();
@@ -73,18 +94,37 @@ function operate(op, prev, curr) {
         return curr;
     }
     [prev, curr] = [prev - 0, curr - 0];
+    let result;
     switch (op) {
         case "+":
-            return prev + curr;
+            result = prev + curr;
+            break;
         case "-":
-            return prev - curr;
+            result = prev - curr;
+            break;
         case "×":
-            return prev * curr;
+            result = prev * curr;
+            break;
         case "÷":
-            return prev / curr;
+            result = prev / curr;
+            break;
         case "%":
-            return prev % curr; // Not intended function, but okei
+            result = prev % curr; // Not intended function, but okei
+            break;
     }
+
+    result = result.toString();
+    // if (result.length > 21) {
+    //     let wholePart, decimalPart;
+    //     [wholePart, decimalPart] = result.split(".");
+    //     if (!decimalPart || wholePart.length > 20) {
+    //         result = "OVERFLOW";
+    //     }
+    //     else {
+    //         result = wholePart + "." + decimalPart.slice(0, 21 - wholePart.length);
+    //     }
+    // }
+    return result;
 }
 
 function clearAll() {
@@ -97,7 +137,11 @@ function clearAll() {
 }
 
 function deleteChar() {
-
+    if (!startNewNum) {
+        displayVal = displayVal.slice(0, -1);
+        if (!displayVal) displayVal = 0;
+        updateDisplay();
+    }
 }
 
 function updateDisplay() {
@@ -114,3 +158,24 @@ function isOperator(char) {
 }
 
 window.onload = setup;
+
+
+
+function Debs() {
+    if (["9999", "991999", "9091999", "0.9091999"].includes(displayVal)) {
+        displayVal = "Thanks for all your support! I love you babe 😘❤"
+        updateDisplay();
+        displayVal = "0";
+        history.innerText = "";
+
+        clearTimeout(heartGrow);
+        heartGrow();
+        setTimeout(heartGrow, 3000);
+
+        return true;
+    }
+}
+
+function heartGrow() {
+    document.querySelector(".heart").classList.toggle("heartGrow")
+}
